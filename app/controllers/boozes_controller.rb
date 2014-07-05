46515534b7c30edb1fac3241dbf5d2e314eef486
Justin Veirs <justinveirs@gmail.com>
2014-07-04 19:41:58 -0600

    controller up

---
+++ app/controllers/boozes_controller.rb
@@ -0,0 +1,7 @@
+class BoozesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
---
+++ test/helpers/boozes_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class BoozesHelperTest < ActionView::TestCase
+end
